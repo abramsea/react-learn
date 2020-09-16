d4f797847f6353a32316978a21d331edddcc425f
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-09-16 18:57:35 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg'
+import Message from './components/Message.js'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message 
+        name="Vasya"
+        logo={logo}
+        title="My Title"
+        text="Hello"
+       />
+      <Message 
+        name="Katya"
+        logo={logo}
+        title="Katya title"
+        text="Yo"
+       />
+      <Message 
+        name="Sasha"
+        logo={logo}
+        title="Sasha title"
+        text="Sasha text"
+       />
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Message.css'
+
+
+class Message extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isLiked: false,
+      counter: 0
+    };
+  }
+
+    render() {
+        const { name, logo, title, text } = this.props;
+        const { isLiked, counter } = this.state;
+        const date = new Date();
+        const displayDate = `${date.getDate()}/${date.getMonth() +1}/${date.getFullYear()}`
+
+        const btnClassName = `message-header-button ${isLiked ? 'is-active' : ''}`
+
+        return (
+            <div className="message">
+              <div className="message-header">
+                <img src={logo} alt="" className="message-header-image" />
+                <div className="message-header-text_block">
+                  <p className="message-header-title"> 
+                      {name}
+                  </p>
+                  <span className="message-header-date">
+                    {displayDate}
+                  </span>
+                </div>
+                <span className="message-header-counter">
+                    {counter}
+                  </span>
+                  <button className={btnClassName}
+                    onClick={() => this.setState( {isLiked: !isLiked, counter: counter +1})}
+                  />
+                  
+              </div>
+              <div className="message=body">
+                  <h2 className="message-body-title">
+                    {title}
+                  </h2>
+                  <p className="message-body-content">
+                      {text}
+                  </p>
+              </div>
+            </div>
+        );
+    } 
+  }
+
+
+export default Message;
