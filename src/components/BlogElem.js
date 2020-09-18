24d7119b245903f8111fa75b344326f73a3896b8
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-09-18 17:42:47 +0300

    pic added

--- src/components/BlogElem.js
+++ src/components/BlogElem.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './BlogElem.css';
 import axios from 'axios';
 
-const API = 'http://www.randomtext.me/api/gibberish/'
+const API = 'http://www.randomtext.me/api/gibberish/';
+
 
 class BlogElem extends React.Component {
 
@@ -19,13 +20,16 @@ class BlogElem extends React.Component {
           .then((res) => {
             this.setState( {blogData: res.data.text_out });
           });
-
       }
 
 
     render() {
         return (
             <div className="blog-elem">
+                <img className="blog-elem-pic" src='https://picsum.photos/400/300' />
+                <h2 className="blog-elem-title">
+                    Random title
+                </h2>
                 {this.state.blogData}
             </div>
         )
